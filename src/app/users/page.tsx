0b06b11f08b36bb6602b70c503af816cb616be6e
Toxa1704 <Toxa1704@outlook.com>
2024-08-29 19:25:27 +0300

    main

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,13 +1,47 @@
-'use client'
-import fetchData from "../dataUsers";
+import usersData from "@/component/_usersData";
+import styles from "@/css/usersData.module.css"
 
-export default function test() {
- fetchData()
+
+
+
+
+
+const test: React.FC = () => {
   return (
     <div>
-      <h1>Data from JSON file</h1>
-      <ul id="user-list"></ul>
+      <h1>Users</h1>
+
+
+      
+      <div className={styles.usersList}>
+      <ul>
+        <div>Full Name</div>
+        {usersData.map((user, index) => (
+          <li key={index}>{user.full_name}</li>
+        ))}
+      </ul>
+      <ul>
+      <div>Department</div>
+        {usersData.map((user, index) => (
+          <li key={index}>{user.department}</li>
+        ))}
+      </ul>
+      <ul>
+      <div>Country</div>
+        {usersData.map((user, index) => (
+          <li key={index}>{user.country}</li>
+        ))}
+      </ul>
+      <ul>
+      <div>Status</div>
+        {usersData.map((user, index) => (
+          <li key={index}>{user.status}</li>
+        ))}
+      </ul>
+      </div>
     </div>
   );
-}
+};
+
+export default test;
 
---
+++ src/component/_usersData.tsx
@@ -0,0 +1,42 @@
+interface User {
+    full_name: string;
+    country: string;
+    department: string;
+    status: string;
+  }
+  
+  const usersData: User[] = [
+    {
+      full_name: "John Doe",
+      country: "USA",
+      department: "Engineering",
+      status: "Active"
+    },
+    {
+      full_name: "Jane Smith",
+      country: "Canada",
+      department: "Marketing",
+      status: "Inactive"
+    },
+    {
+      full_name: "Ivan Petrov",
+      country: "Egypt",
+      department: "Sales",
+      status: "Active"
+    },
+    {
+      full_name: "Maria Garcia",
+      country: "Spain",
+      department: "HR",
+      status: "Active"
+    },
+    {
+      full_name: "Liu Wei",
+      country: "China",
+      department: "Finance",
+      status: "Inactive"
+    }
+  ];
+  
+
+  export default usersData;
