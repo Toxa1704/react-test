12a3981f7fa725697b69034dd8f8a3f91d9c15b5
Toxa1704 <Toxa1704@outlook.com>
2024-09-05 16:01:40 +0300

    main

--- src/component/_popUpAddUser.tsx
+++ src/component/_popUpAddUser.tsx
@@ -3,7 +3,9 @@
 import usersData from "./_usersData";
 import dataCountries from "./_dataCountries";
 import MenuDropdownDepartment from "@/component/_addUserDepartment"
+import MenuDropdownContry from "@/component/_addUserCountry";
 import styles from "@/css/popUpAddUser.module.css";
+import MenuDropdownCountry from "@/component/_addUserCountry";
 
 const PopUpAddUser = () => {
 
@@ -20,12 +22,12 @@ const PopUpAddUser = () => {
                         <div className={styles.inputForm}>
                             <div className={styles.inputFormTitle}>Department</div>
                             <MenuDropdownDepartment />
-                            </div>
-                        
-                        <div className={styles.inputForm}>
-                            <label>Country</label>
-                            <input className={styles.formInput} />
                         </div>
+                        <div className={styles.inputForm}>
+                            <div className={styles.inputFormTitle}>Country</div>
+                            <MenuDropdownCountry />
+                        </div>
+
                         <div className={styles.inputForm}>
                             <label>Status</label>
                             <input className={styles.formInput} />
