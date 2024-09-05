4d97d0181acc84eed1088dacacb562becd40161d
Toxa1704 <Toxa1704@outlook.com>
2024-09-05 15:24:04 +0300

    main

--- src/component/_popUpAddUser.tsx
+++ src/component/_popUpAddUser.tsx
@@ -1,44 +1,44 @@
-'use client'
+"use client";
 
-
-
-import usersData from "./_usersData"
-import dataDepartments from "./_dataDepartments"
-import dataCountries from "./_dataCountries"
-import styles from "@/css/popUpAddUser.module.css"
+import usersData from "./_usersData";
+import dataCountries from "./_dataCountries";
+import MenuDropdownDepartment from "@/component/_addUserDepartment"
+import styles from "@/css/popUpAddUser.module.css";
 
 const PopUpAddUser = () => {
+
     return (
         <div className={styles.popUpWrapper}>
             <div className={styles.addUserTitle}>Add User</div>
             <div className={styles.addUserForm}>
-                <form >
+                <form>
                     <div className={styles.formWrapper}>
                         <div className={styles.inputForm}>
                             <label htmlFor="fullName">Full Name</label>
-                            <input type="text" id="fullName" className={styles.formInput}/>
+                            <input type="text" id="fullName" className={styles.formInput} />
                         </div>
                         <div className={styles.inputForm}>
-                            <label >Department</label>
-                            <input className={styles.formInput}/>
-                        </div>
+                            <div className={styles.inputFormTitle}>Department</div>
+                            <MenuDropdownDepartment />
+                            </div>
+                        
                         <div className={styles.inputForm}>
                             <label>Country</label>
-                            <input className={styles.formInput}/>
+                            <input className={styles.formInput} />
                         </div>
                         <div className={styles.inputForm}>
                             <label>Status</label>
-                            <input className={styles.formInput}/>
+                            <input className={styles.formInput} />
                         </div>
-                    </div> 
-                        <div className={styles.btnForm}>
-                            <input type="reset" className={`${styles.popUpAddUser} ${styles.cansel}`} value={"Canсel"} />
-                            <input type="submit" className={`${styles.popUpAddUser} ${styles.addUser}`} value={"Add User"} />
-                        </div>
+                    </div>
+                    <div className={styles.btnForm}>
+                        <input type="reset" className={`${styles.popUpAddUser} ${styles.cansel}`} value={"Cancel"} />
+                        <input type="submit" className={`${styles.popUpAddUser} ${styles.addUser}`} value={"Add User"} />
+                    </div>
                 </form>
             </div>
-        </div >
-    )
-}
+        </div>
+    );
+};
 
-export default PopUpAddUser
+export default PopUpAddUser;
