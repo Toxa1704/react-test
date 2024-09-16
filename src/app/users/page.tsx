5b69bacf30602db8452e1f31ac888b15b80c1d59
Toxa1704 <Toxa1704@outlook.com>
2024-09-16 13:44:35 +0300

    main

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -88,9 +88,7 @@ const Users: React.FC = () => {
     return usersData;
   }
 
-  console.log(addUser);
-  let addUserData = JSON.stringify(updateUsersData(usersData, addUser));
-  console.log(addUserData);
+  JSON.stringify(updateUsersData(usersData, addUser));
 
 
   const filteredUsers = usersData.filter(user => {
@@ -141,9 +139,11 @@ const Users: React.FC = () => {
                   {filters.country && <td>{user.country}</td>}
                   {filters.status && <td>{user.status}</td>}
                   <td>
+                    <div className={styles.deleteUser}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5E626B">
                       <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                     </svg>
+                    </div>
                   </td>
                 </tr>
               ))}
